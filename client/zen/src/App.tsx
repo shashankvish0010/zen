429ff9de0436992cb338e4d663b34987ce3188ac
Shashank Vishwakarma <shashank000s4@gmail.com>
2023-09-22 19:02:34 +0530

    Added Basic Headers

--- client/zen/src/App.tsx
+++ client/zen/src/App.tsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import {Routes, Route} from 'react-router-dom'
 import About from './pages/About'
+import Header from './components/Header'
 
 const App: React.FC = () => {
   return (
+    <>
+    <Header/>
     <Routes>
       <Route path='/about' element={<About/>} />
     </Routes>
+    </>
   )
 }
 
--- client/zen/src/components/Header.tsx
+++ client/zen/src/components/Header.tsx
@@ -1,8 +1,24 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import { Icon } from '@iconify/react';
 
 const Header: React.FC = () => {
   return (
-    <div>Header</div>
+    <div className='h-[8vh] w-[100vw] flex flex-row justify-around items-center shadow'>
+      <Link to='/'><div className='text-2xl flex gap-1'>
+      <Icon icon="simple-icons:zenn" color='#9333ea'/>
+        <h1 className='logo'>ZEN</h1>
+      </div>
+      </Link>
+      <div className='w-[30vw]'>
+        <ul className='flex justify-between gap-2 items-center font-semibold'>
+          <Link to='/'>Home</Link>
+          <Link to='/'>About</Link>
+          <Link to='/'>Contact</Link>
+          <Link to='/'><button className='bg-purple-500 p-1 text-white'>Login/Signup</button></Link>
+        </ul>
+      </div>
+    </div>
   )
 }
 
