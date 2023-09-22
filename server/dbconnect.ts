885e9e3334570fdfaae354aeefd2721e3a743b58
Shashank Vishwakarma <shashank000s4@gmail.com>
2023-09-22 19:35:51 +0530

    db user

--- server/dbconnect.ts
+++ server/dbconnect.ts
@@ -1,4 +1,6 @@
 import {Pool} from 'pg'
+import dotenv from 'dotenv'
+dotenv.config()
 
 const localdb = `postgres://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST_LOCAL}:${process.env.DB_PORT}/${process.env.DB_NAME}`
 
