0585bce430a8a7b44a4e44f9640c6af9ec158423
Shashank Vishwakarma <shashank000s4@gmail.com>
2023-09-25 22:08:19 +0530

    added

--- client/zen/src/components/Footer.tsx
+++ client/zen/src/components/Footer.tsx
@@ -1,8 +1,41 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import { Icon } from '@iconify/react'
 
 const Footer: React.FC = () => {
   return (
-    <div>Footer</div>
+    <div className='mt-[max] shadow-md border p-5 flex flex-col justify-evenly items-center h-max w-[100vw]'>
+      <div className='flex md:flex-row flex-col gap-3 items-center justify-between w-[100vw] px-8'>
+        <Link to='/'>
+          <div className='text-xl flex gap-1'>
+            <Icon icon="simple-icons:zenn" color='#9333ea' />
+            <h1 className='logo'>ZEN</h1>
+          </div>
+        </Link>
+        <div className='h-max w-max'>
+          <ul className='md:w-[35vw] w-[80vw] text-base font-semibold h-max flex flex-row items-center justify-around'>
+            <Link className='hover:text-purple-600' to='/about'>About</Link>
+            <Link className='hover:text-purple-600' to='/contact'>Contact</Link>
+            <Link className='hover:text-purple-600' to='/register'>Sign Up</Link>
+          </ul>
+        </div>
+      </div>
+      <div className='w-max h-max flex flex-row items-center mt-5 gap-6'>
+        <a href='https://github.com/shashankvish0010'><Icon className='shadow-xl cursor-pointer hover:shadow-2xl' height='3vh' icon="mdi:github" /></a>
+        <a href='https://www.linkedin.com/in/shashank-vishwakarma-650555283/'><Icon className='shadow-xl cursor-pointer hover:shadow-2xl' height='3vh' icon="devicon:linkedin" /></a>
+        <a href='https://twitter.com/ShashankVis001'><Icon className='shadow-xl cursor-pointer hover:shadow-2xl' height='3vh' icon="line-md:twitter-x-alt" /></a>
+      </div>
+      <div className='flex md:flex-row flex-col items-center w-screen h-max justify-center gap-1 mt-5'>
+        <Link to='/'>
+          <div className='text-base flex items-start gap-1'>
+            <Icon icon="simple-icons:zenn" color='#9333ea' />
+            <h1 className='logo'>ZEN</h1>
+          </div>
+        </Link>
+        <p className='flex md:flex-row flex-col justify-center gap-1 text-center w-max font-medium'>&copy; Designed and Developed by <span className='text-purple-600  font-semibold'
+        > <a href="https://www.linkedin.com/in/shashank-vishwakarma-650555283/">Shashank Vishwakarma</a></span></p>
+      </div>
+    </div>
   )
 }
 
--- client/zen/src/components/Header.tsx
+++ client/zen/src/components/Header.tsx
@@ -1,4 +1,4 @@
-import React, {useContext} from 'react'
+import React, { useContext } from 'react'
 import { Link } from 'react-router-dom'
 import { Icon } from '@iconify/react';
 import { UserContext } from '../context/Userauth';
@@ -7,20 +7,21 @@ const Header: React.FC = () => {
   const usercontext = useContext(UserContext)
   return (
     <div className='h-[10vh] w-[100vw] flex flex-row justify-around items-center shadow'>
-      <Link to='/'><div className='text-2xl flex gap-1'>
-      <Icon icon="simple-icons:zenn" color='#9333ea'/>
-        <h1 className='logo'>ZEN</h1>
-      </div>
+      <Link to='/'>
+        <div className='text-2xl flex gap-1'>
+          <Icon icon="simple-icons:zenn" color='#9333ea' />
+          <h1 className='logo'>ZEN</h1>
+        </div>
       </Link>
       <div className='w-[30vw]'>
         <ul className='hidden md:flex justify-between gap-2 items-center font-semibold'>
           <Link to='/'>Home</Link>
           <Link to='/'>About</Link>
           <Link to='/'>Contact</Link>
-          {  usercontext?.login?.status == false ?
-          <Link to='/signup'><button className='bg-purple-600 p-1.5 text-white rounded-sm'>Login/Signup</button></Link>
-          :
-          <button onClick={()=>{usercontext?.dispatch({type: "LOGOUT"})}} className='bg-purple-600 p-1.5 text-white rounded-sm'>Logout</button>
+          {usercontext?.login?.status == false ?
+            <Link to='/signup'><button className='bg-purple-600 p-1.5 text-white rounded-sm'>Login/Signup</button></Link>
+            :
+            <button onClick={() => { usercontext?.dispatch({ type: "LOGOUT" }) }} className='bg-purple-600 p-1.5 text-white rounded-sm'>Logout</button>
           }
         </ul>
       </div>
