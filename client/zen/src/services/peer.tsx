b610996fde5b4b7f5dd0d55be7f60b755b1fe58f
Shashank Vishwakarma <shashank000s4@gmail.com>
2023-09-28 15:57:59 +0530

    added

--- client/zen/src/services/peer.tsx
+++ client/zen/src/services/peer.tsx
@@ -28,23 +28,6 @@ class Peerconnection {
         }
     }
 
-    // async generateAnswer (offer: RTCSessionDescriptionInit) {
-    //     console.log("enteranswer");
-    //         if(this.peer){
-    //             try {
-
-    //             await this.peer.setRemoteDescription(offer)
-    //             const answer = await this.peer.createAnswer()
-    //             console.log(answer);
-    //             await this.peer.setLocalDescription(new RTCSessionDescription(answer))
-    //             return answer
-    //         }
-    //      catch (error) {
-    //         console.log('genarateAnswer', error);
-    //     }
-    // }
-    // }
-
     async generateAnswer(offer: RTCSessionDescriptionInit) {
         console.log("Entering generateAnswer");
         console.log("Received offer:", offer);
@@ -66,7 +49,6 @@ class Peerconnection {
         }
     }
     
-
     async setRemoteDescription (answer: RTCSessionDescriptionInit) {
         console.log("enterremote");
         try {
@@ -81,68 +63,3 @@ class Peerconnection {
 }
 
 export default new Peerconnection()
-
-// class Peerconnection {
-//     peer: any;
-
-//     constructor() {
-//         if (!this.peer) {
-//             this.peer = new RTCPeerConnection({
-//                 iceServers: [
-//                     {
-//                         urls: [
-//                             "stun:stun.l.google.com:19302",
-//                             "stun:global.stun.twilio.com:3478"
-//                         ]
-//                     }
-//                 ]
-//             });
-//         }
-//     }
-
-//     async generateOffer() {
-//         console.log("Entering generateOffer");
-//         if (this.peer) {
-//             try {
-//                 const offer = await this.peer.createOffer();
-//                 await this.peer.setLocalDescription(new RTCSessionDescription(offer));
-//                 console.log("Offer created and local description set.");
-//                 return offer;
-//             } catch (error) {
-//                 console.error("Error generating offer:", error);
-//             }
-//         }
-//     }
-
-//     async generateAnswer(offer: RTCSessionDescriptionInit) {
-//         console.log("Entering generateAnswer");
-//         console.log("Received offer:", offer);
-
-//         if (this.peer) {
-//             try {
-//                 await this.peer.setRemoteDescription(new RTCSessionDescription(offer));
-//                 console.log("Remote description set with offer.");
-//                 const answer = await this.peer.createAnswer()
-//                 await this.peer.setLocalDescription(new RTCSessionDescription(answer));
-//                 console.log("Answer created and local description set.");
-//                 return answer;
-//             } catch (error) {
-//                 console.error("Error generating answer:", error);
-//             }
-//         }
-//     }
-
-//     async setRemoteDescription(answer: RTCSessionDescriptionInit) {
-//         console.log("Entering setRemoteDescription");
-//         if (this.peer) {
-//             try {
-//                 await this.peer.setRemoteDescription(new RTCSessionDescription(answer));
-//                 console.log("Remote description set with answer.");
-//             } catch (error) {
-//                 console.error("Error setting remote description:", error);
-//             }
-//         }
-//     }
-// }
-
-// export default new Peerconnection();
