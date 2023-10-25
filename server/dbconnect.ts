aad275cc034d00aa4e7c881e31019ea4c1266834
Shashank Vishwakarma <shashank000s4@gmail.com>
2023-10-25 16:10:38 +0530

    Production Postgres Db added from vercel

--- server/dbconnect.ts
+++ server/dbconnect.ts
@@ -4,10 +4,8 @@ dotenv.config()
 
 const localdb = `postgres://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST_LOCAL}:${process.env.DB_PORT}/${process.env.DB_NAME}`
 
-const proddb = `postgres://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST_LOCAL}:${process.env.DB_PORT}/${process.env.DB_NAME}`
-
 const pool = new Pool({
-   connectionString : process.env.NODE === "production" ? proddb : localdb
+   connectionString : process.env.NODE === "production" ? process.env.proddb + "?sslmode=require" : localdb
 })
 
 export default pool
