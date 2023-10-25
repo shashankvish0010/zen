ca14b1a7b0d414ed9e55c485d59aab1b2546cbf5
Shashank Vishwakarma <shashank000s4@gmail.com>
2023-10-25 17:00:45 +0530

    added NODE ENV

--- server/dbconnect.ts
+++ server/dbconnect.ts
@@ -5,7 +5,7 @@ dotenv.config()
 const localdb = `postgres://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST_LOCAL}:${process.env.DB_PORT}/${process.env.DB_NAME}`
 
 const pool = new Pool({
-   connectionString : process.env.NODE === "production" ? process.env.proddb : localdb,
+   connectionString : process.env.NODE_ENV === "production" ? process.env.proddb : localdb,
    ssl: {
       rejectUnauthorized: false,
     },
