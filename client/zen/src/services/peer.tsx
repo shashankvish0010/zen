043f9b387a1074e34f50a6bf6ef9824ba9bd4fb4
Shashank Vishwakarma <shashank000s4@gmail.com>
2023-10-25 18:56:42 +0530

    added new Ice Server SYN

--- client/zen/src/services/peer.tsx
+++ client/zen/src/services/peer.tsx
@@ -5,14 +5,26 @@ class Peerconnection {
         if (!this.peer) {
 
             this.peer = new RTCPeerConnection({
-                iceServers: [
-                    { urls: 'stun:stun.l.google.com:19302' },
+                "iceServers": [
                     {
-                        username: "dc2d2894d5a9023620c467b0e71cfa6a35457e6679785ed6ae9856fe5bdfa269",
-                        credential: "tE2DajzSJwnsSbc123",
-                        urls: "turn:global.turn.twilio.com:3478?transport=udp"
+                      "urls": "stun:global.stun.twilio.com:3478"
                     },
-                ],
+                    {
+                      "username": "dc2d2894d5a9023620c467b0e71cfa6a35457e6679785ed6ae9856fe5bdfa269",
+                      "credential": "tE2DajzSJwnsSbc123",
+                      "urls": "turn:global.turn.twilio.com:3478?transport=udp"
+                    },
+                    {
+                      "username": "dc2d2894d5a9023620c467b0e71cfa6a35457e6679785ed6ae9856fe5bdfa269",
+                      "credential": "tE2DajzSJwnsSbc123",
+                      "urls": "turn:global.turn.twilio.com:3478?transport=tcp"
+                    },
+                    {
+                      "username": "dc2d2894d5a9023620c467b0e71cfa6a35457e6679785ed6ae9856fe5bdfa269",
+                      "credential": "tE2DajzSJwnsSbc123",
+                      "urls": "turn:global.turn.twilio.com:443?transport=tcp"
+                    }
+                  ],
                 // iceServers: [
                 //     {
                 //       urls: "stun:stun.relay.metered.ca:80",
