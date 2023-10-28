a3576dce4c883fdd9735c3177291763e5bbe67ef
Shashank Vishwakarma <shashank000s4@gmail.com>
2023-10-28 23:36:39 +0530

    Added Metered Ice

--- client/zen/src/services/peer.tsx
+++ client/zen/src/services/peer.tsx
@@ -8,9 +8,7 @@ class Peerconnection {
             this.peer = new RTCPeerConnection({
                 iceServers: [
                     {
-                        urls: "stun:stun.services.mozilla.com",
-                        username: "louis@mozilla.com",
-                        credential: "webrtcdemo",
+                        "urls": "stun:stun.relay.metered.ca:80",
                     }
                 ]
                 // "iceServers": [
