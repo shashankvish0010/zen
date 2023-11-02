da35cf2209e56b6b0519699df5dfae8a511b0698
Shashank Vishwakarma <shashank000s4@gmail.com>
2023-11-02 20:45:19 +0530

    DEBUGGING - Added Updated Mediacodecs from doc.

--- server/dist/src/app.js
+++ server/dist/src/app.js
@@ -65,19 +65,21 @@ let sender;
 let sendersOffer;
 const mediacodecs = [
     {
-        kind: 'audio',
-        mimeType: 'audio/opus',
+        kind: "audio",
+        mimeType: "audio/opus",
         clockRate: 48000,
         channels: 2
     },
     {
-        kind: 'video',
-        mimeType: 'video/VP8',
+        kind: "video",
+        mimeType: "video/H264",
         clockRate: 90000,
         parameters: {
-            'x-google-start-bitrate': 1000,
+            "packetization-mode": 1,
+            "profile-level-id": "42e01f",
+            "level-asymmetry-allowed": 1
         }
-    },
+    }
 ];
 io.on('connection', (socket) => {
     // --------------------------------------- WebSocket connection for Zen Call || Video Call --------------------------------- 
--- server/src/app.ts
+++ server/src/app.ts
@@ -30,20 +30,23 @@ let sendersOffer: any;
 
 const mediacodecs: any = [
     {
-        kind: 'audio',
-        mimeType: 'audio/opus',
+        kind: "audio",
+        mimeType: "audio/opus",
         clockRate: 48000,
         channels: 2
     },
     {
-        kind: 'video',
-        mimeType: 'video/VP8',
+        kind: "video",
+        mimeType: "video/H264",
         clockRate: 90000,
-        parameters: {
-            'x-google-start-bitrate': 1000,
+        parameters:
+        {
+            "packetization-mode": 1,
+            "profile-level-id": "42e01f",
+            "level-asymmetry-allowed": 1
         }
-    },
-]
+    }
+];
 
 
 io.on('connection', (socket) => {
@@ -92,7 +95,7 @@ io.on('connection', (socket) => {
                 console.log("worker created");
             })
         } catch (error) {
-         console.log(error);
+            console.log(error);
         }
     })
 
