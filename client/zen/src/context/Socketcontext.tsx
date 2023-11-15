2de48e0598c130a560289c326030072147c92a6b
Shashank Vishwakarma <shashank000s4@gmail.com>
2023-11-16 00:39:34 +0530

    Added changes

--- client/zen/src/context/Socketcontext.tsx
+++ client/zen/src/context/Socketcontext.tsx
@@ -210,7 +210,7 @@ const SocketProvider = (props: any) => {
     }
 
     const addLocalStream = async (stream: any | MediaStream) => {
-        let track = stream.getTracks()[0]
+        let track = stream.getTracks()[1]
         console.log(track);
         
         params = {
