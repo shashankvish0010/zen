287267706f7236eee1705cbe5b2accb43b4b5587
Shashank Vishwakarma <shashank000s4@gmail.com>
2023-12-14 02:37:38 +0530

    Updated JS

--- server/src/app.ts
+++ server/src/app.ts
@@ -141,7 +141,7 @@ io.on('connection', (socket) => {
             
             let transport = await mediasoupRouter.createWebRtcTransport(WebRTCOptions)
             console.log(transport);
-            
+        
             transport.on('dtlsstatechnage', (dtlsState: any) => {
                 if (dtlsState === 'closed') {
                     transport.close()
