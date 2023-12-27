adb19e29dd3b4a10eb4e55418d766da21e298442
Shashank Vishwakarma <shashank000s4@gmail.com>
2023-12-27 22:37:32 +0530

    cors fixed

--- server/src/app.ts
+++ server/src/app.ts
@@ -16,9 +16,7 @@ import { RouterOptions } from "mediasoup/node/lib/types"
 //     cert: fs.readFileSync(certfile, 'utf-8')
 // }
 const server = http.createServer(app)
-app.use(cors({
-    origin: "https://zen-gamma.vercel.app"
-}))
+app.use(cors())
 const io = new Server(server, ({
     cors: {
         origin: 'https://zen-gamma.vercel.app',
