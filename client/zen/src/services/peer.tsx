e1827fb645e8c238c4cec111642ebdb218ad43bd
Shashank Vishwakarma <shashank000s4@gmail.com>
2024-01-02 15:12:52 +0530

    Added

--- client/zen/src/services/peer.tsx
+++ client/zen/src/services/peer.tsx
@@ -7,31 +7,28 @@ class Peerconnection {
 
             this.peer = new RTCPeerConnection({
                 iceServers: [
-                    // {
-                    //     urls: "stun:stun.relay.metered.ca:80"
-                    // },
-                    // {
-                    //     urls: "turn:a.relay.metered.ca:80",
-                    //     username: "9feb6522d3b0d94f7d4bdc53",
-                    //     credential: "NBrSkTE3xWZTzRlx"
-                    // },
-                    // {
-                    //     urls: "turn:a.relay.metered.ca:80?transport=tcp",
-                    //     username: "9feb6522d3b0d94f7d4bdc53",
-                    //     credential: "NBrSkTE3xWZTzRlx"
-                    // },
-                    // {
-                    //     urls: "turn:a.relay.metered.ca:443",
-                    //     username: "9feb6522d3b0d94f7d4bdc53",
-                    //     credential: "NBrSkTE3xWZTzRlx"
-                    // },
-                    // {
-                    //     urls: "turn:a.relay.metered.ca:443?transport=tcp",
-                    //     username: "9feb6522d3b0d94f7d4bdc53",
-                    //     credential: "NBrSkTE3xWZTzRlx"
-                    // }
                     {
-                        urls: "stun:stun.relay.backups.cz"
+                        urls: "stun:stun.relay.metered.ca:80"
+                    },
+                    {
+                        urls: "turn:a.relay.metered.ca:80",
+                        username: "9feb6522d3b0d94f7d4bdc53",
+                        credential: "NBrSkTE3xWZTzRlx"
+                    },
+                    {
+                        urls: "turn:a.relay.metered.ca:80?transport=tcp",
+                        username: "9feb6522d3b0d94f7d4bdc53",
+                        credential: "NBrSkTE3xWZTzRlx"
+                    },
+                    {
+                        urls: "turn:a.relay.metered.ca:443",
+                        username: "9feb6522d3b0d94f7d4bdc53",
+                        credential: "NBrSkTE3xWZTzRlx"
+                    },
+                    {
+                        urls: "turn:a.relay.metered.ca:443?transport=tcp",
+                        username: "9feb6522d3b0d94f7d4bdc53",
+                        credential: "NBrSkTE3xWZTzRlx"
                     }
                 ],
             }
