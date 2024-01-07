fc9f76c976d0cd54682732627e12f0ce6aa2498d
Shashank Vishwakarma <shashank000s4@gmail.com>
2024-01-07 21:21:23 +0530

    debug: consume resume  action with consumer resume

--- client/zen/src/pages/LiveStream.tsx
+++ client/zen/src/pages/LiveStream.tsx
@@ -2,24 +2,28 @@ import React, { useContext, useEffect } from 'react'
 import ReactPlayer from 'react-player'
 import { Socketcontext } from '../context/Socketcontext'
 
-
 const LiveStream: React.FC = () => {
-    const livestreamContext = useContext(Socketcontext)
+    const livestreamContext = useContext(Socketcontext);
+
     useEffect(() => {
-        console.log("media",livestreamContext?.liveStream);
-    }, [livestreamContext?.liveStream])
+        console.log("media", livestreamContext?.liveStream);
+    }, [livestreamContext?.liveStream]);
+
     return (
         <div className='h-screen w-screen flex items-center justify-center'>
             <div>
-                {
-                    livestreamContext ? 
-                    <ReactPlayer playing url={livestreamContext.liveStream} height={400} width={500} />
-                    : null
-                }
+                {livestreamContext ? (
+                    <ReactPlayer
+                        playing
+                        url={livestreamContext?.liveStream}
+                        height={400}
+                        width={500}
+                    />
+                ) : null}
             </div>
-            <button onClick={() => livestreamContext?.linkStream() }>Click</button>
+            <button onClick={() => livestreamContext?.linkStream()}>Click</button>
         </div>
-    )
-}
+    );
+};
 
 export default LiveStream
