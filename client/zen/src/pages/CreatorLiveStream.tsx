53bbd4c299811f0061f59b62415b672c62d6e028
Shashank Vishwakarma <shashank000s4@gmail.com>
2024-01-07 19:45:06 +0530

    Debug: live stream tracks on live page

--- client/zen/src/pages/CreatorLiveStream.tsx
+++ client/zen/src/pages/CreatorLiveStream.tsx
@@ -17,6 +17,13 @@ const CreatorLiveStream: React.FC = () => {
                     <ReactPlayer playing url={livestreamcontext?.localLiveStream} height={400} width={500} />
                 }
             </div>
+            <div>
+                {
+                    livestreamcontext?.liveStream &&
+                    <ReactPlayer playing url={livestreamcontext.liveStream} height={400} width={500} />
+                }
+            </div>
+            <button onClick={() => { livestreamcontext?.linkStream() }}>Click</button>
         </div>
     )
 
