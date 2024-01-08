f5222ae7a98e8446a0fd69b3a09086bc8fc61b8e
Shashank Vishwakarma <shashank000s4@gmail.com>
2024-01-08 20:42:49 +0530

    Debug: Updating the stream setting methods

--- client/zen/src/pages/LiveStream.tsx
+++ client/zen/src/pages/LiveStream.tsx
@@ -1,29 +1,59 @@
-import React, { useContext, useEffect } from 'react'
-import ReactPlayer from 'react-player'
+import React, { useContext, useEffect, useRef } from 'react'
+// import ReactPlayer from 'react-player'
 import { Socketcontext } from '../context/Socketcontext'
 
+// const LiveStream: React.FC = () => {
+//     const livestreamContext = useContext(Socketcontext);
+
+//     useEffect(() => {
+//         console.log("media", livestreamContext?.liveStream);
+//     }, [livestreamContext?.liveStream]);
+
+//     return (
+//         <div className='h-screen w-screen flex items-center justify-center'>
+//             <div>
+//                 {livestreamContext?.liveStream && (
+//                     <ReactPlayer
+//                         playing
+//                         url={livestreamContext?.liveStream}
+//                         height={400}
+//                         width={500}
+//                     />
+//                 )}
+//             </div>
+//             <button onClick={livestreamContext?.linkStream}>Click</button>
+//         </div>
+//     );
+// };
+
 const LiveStream: React.FC = () => {
-    const livestreamContext = useContext(Socketcontext);
+  const livestreamContext = useContext(Socketcontext);
+  const videoRef = useRef<HTMLVideoElement | null>(null);
 
-    useEffect(() => {
-        console.log("media", livestreamContext?.liveStream);
-    }, [livestreamContext?.liveStream]);
+  useEffect(() => {
+    const videoElement = videoRef.current;
 
-    return (
-        <div className='h-screen w-screen flex items-center justify-center'>
-            <div>
-                {livestreamContext?.liveStream && (
-                    <ReactPlayer
-                        playing
-                        url={livestreamContext?.liveStream}
-                        height={400}
-                        width={500}
-                    />
-                )}
-            </div>
-            <button onClick={livestreamContext?.linkStream}>Click</button>
-        </div>
-    );
+    if (livestreamContext?.liveStream && videoElement) {
+      videoElement.srcObject = livestreamContext.liveStream;
+      videoElement.play().catch((error) => console.error('Error playing video:', error));
+    }
+
+    return () => {
+      // Cleanup
+      if (videoElement) {
+        videoElement.srcObject = null;
+      }
+    };
+  }, [livestreamContext?.liveStream]);
+
+  return (
+    <div className='h-screen w-screen flex items-center justify-center'>
+      <div>
+        <video ref={videoRef} height={400} width={500} autoPlay playsInline muted={false}></video>
+      </div>
+      <button onClick={livestreamContext?.linkStream}>Click</button>
+    </div>
+  );
 };
 
 export default LiveStream
