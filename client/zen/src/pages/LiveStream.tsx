9fcb17888c1866c758e240b25f2b9f5d903739d6
Shashank Vishwakarma <shashank000s4@gmail.com>
2024-01-20 02:15:21 +0530

    fixed

--- client/zen/src/pages/LiveStream.tsx
+++ client/zen/src/pages/LiveStream.tsx
@@ -1,51 +1,29 @@
-import React, { useContext, useMemo, useState } from 'react'
+import React, { useContext, useEffect } from 'react'
 import ReactPlayer from 'react-player'
 import { Socketcontext } from '../context/Socketcontext'
 
-// const LiveStream: React.FC = () => {
-//     const livestreamContext = useContext(Socketcontext);
-
-//     useEffect(() => {
-//         console.log("media", livestreamContext?.liveStream);
-//     }, [livestreamContext?.liveStream]);
-
-//     return (
-//         <div className='h-screen w-screen flex items-center justify-center'>
-//             <div>
-//                 {livestreamContext?.liveStream && (
-//                     <ReactPlayer
-//                         playing
-//                         url={livestreamContext?.liveStream}
-//                         height={400}
-//                         width={500}
-//                     />
-//                 )}
-//             </div>
-//             <button onClick={livestreamContext?.linkStream}>Click</button>
-//         </div>
-//     );
-// };
-
 const LiveStream: React.FC = () => {
-  const livestreamContext = useContext(Socketcontext);
-  const [key, setKey] = useState<boolean>(false)
+    const livestreamContext = useContext(Socketcontext);
 
-  useMemo(()=> {
-    livestreamContext?.liveStream ? setKey(true) : null
-  }, [])
+    useEffect(() => {
+        console.log("media", livestreamContext?.liveStream);
+    }, [livestreamContext?.liveStream]);
 
-  return (
-    <div className='h-screen w-screen flex items-center justify-center'>
-      <div>
-        {
-          livestreamContext?.localLiveStream && key == true ?
-          <ReactPlayer playing url={livestreamContext?.localLiveStream} height={400} width={500} />
-          : null
-        }
-      </div>
-      <button onClick={livestreamContext?.linkStream}>Click</button>
-    </div>
-  );
+    return (
+        <div className='h-screen w-screen flex items-center justify-center'>
+            <div>
+                {livestreamContext?.liveStream && (
+                    <ReactPlayer
+                        playing
+                        url={livestreamContext?.liveStream}
+                        height={400}
+                        width={500}
+                    />
+                )}
+            </div>
+            <button onClick={livestreamContext?.linkStream}>Click</button>
+        </div>
+    );
 };
 
 export default LiveStream
