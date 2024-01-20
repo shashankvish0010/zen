f531dff14eb1c0a17583bd1eb4fdf82e2f4a41b6
Shashank Vishwakarma <shashank000s4@gmail.com>
2024-01-20 22:08:22 +0530

    Updated: Live Stream

--- client/zen/src/pages/LiveStream.tsx
+++ client/zen/src/pages/LiveStream.tsx
@@ -1,29 +1,31 @@
-import React, { useContext, useEffect } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import ReactPlayer from 'react-player'
 import { Socketcontext } from '../context/Socketcontext'
 
 const LiveStream: React.FC = () => {
-    const livestreamContext = useContext(Socketcontext);
+  const livestreamContext = useContext(Socketcontext);
+  const [stream, setStream] = useState()
 
-    useEffect(() => {
-        console.log( livestreamContext?.liveStream);
-    }, [livestreamContext?.liveStream]);
+  useEffect(() => {
+     setStream(livestreamContext?.liveStream)
+     console.log(stream); 
+  }, [livestreamContext?.liveStream, stream]);
 
-    return (
-        <div className='h-screen w-screen flex items-center justify-center'>
-            <div>
-                {livestreamContext?.liveStream && (
-                    <ReactPlayer
-                        playing={true}
-                        url={livestreamContext?.liveStream}
-                        height={400}
-                        width={500}
-                    />
-                )}
-            </div>
-            <button onClick={livestreamContext?.linkStream}>Click</button>
-        </div>
-    );
+  return (
+    <div className='h-screen w-screen flex items-center justify-center'>
+      <div>
+        {stream ?
+          <ReactPlayer
+              playing={true}
+              url={stream}
+              height={400}
+              width={500}
+          />
+          : null}
+      </div>
+      <button onClick={livestreamContext?.linkStream}>Click</button>
+    </div>
+  );
 };
 
 export default LiveStream
