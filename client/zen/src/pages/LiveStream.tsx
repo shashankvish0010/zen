fa766a40d5b4eddb357a5ef005de7535dba20b18
Shashank Vishwakarma <shashank000s4@gmail.com>
2024-01-22 22:35:47 +0530

    Fixed

--- client/zen/src/pages/LiveStream.tsx
+++ client/zen/src/pages/LiveStream.tsx
@@ -6,26 +6,39 @@ const LiveStream: React.FC = () => {
   var videoRef = useRef<HTMLVideoElement | any>(null);
 
   useEffect(() => {
-     console.log(livestreamContext?.liveStream); 
-     if (livestreamContext?.liveStream && videoRef.current) {
+    console.log(livestreamContext?.liveStream);
+    const playStream = async () => {
+      if (livestreamContext?.liveStream && videoRef.current) {
+        try {
+          // Check if liveStream is a valid MediaStream
+          if (livestreamContext.liveStream instanceof MediaStream) {
+            // Set the video source object
+            videoRef.current.srcObject = livestreamContext.liveStream;
 
-     videoRef.current.srcObject = livestreamContext?.liveStream;
+            // Play the video
+            await videoRef.current.play();
+          } else {
+            console.error('Invalid MediaStream:', livestreamContext.liveStream);
+          }
+        } catch (error) {
+          console.error('Error playing the video:', error);
+        }
+      }
+    };
+    playStream()
+  }, [livestreamContext?.liveStream]);
 
-     videoRef.current.play().catch((error: any) => {
-       console.error('Error playing the video:', error);
-     }); }
-     }, [livestreamContext?.liveStream]);
 
   return (
     <div className='h-screen w-screen flex items-center justify-center'>
       <div>
         {livestreamContext?.liveStream ?
           <video
-          ref={videoRef}
-          autoPlay
-          height={400}
-          width={500}
-        ></video>
+            ref={videoRef}
+            autoPlay
+            height={400}
+            width={500}
+          ></video>
           : null}
       </div>
       <button onClick={livestreamContext?.linkStream}>Click</button>
