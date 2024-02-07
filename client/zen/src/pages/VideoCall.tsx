dc85208d05f703240236f8ae6188d0fd485f6e82
Shashank Vishwakarma <shashank000s4@gmail.com>
2024-02-07 22:05:26 +0530

    Updated:

--- client/zen/src/pages/VideoCall.tsx
+++ client/zen/src/pages/VideoCall.tsx
@@ -11,39 +11,31 @@ const VideoCall: React.FC = () => {
   // }, [socketcontext]);
 
   return (
-    <div className='h-screen bg-purple-600 w-screen relative flex flex-col justify-center items-center gap-5 p-3'>
-        <div className='h-max w-max p-2 ml-[80%] mb-[70%] absolute'>
-          {socketcontext?.LocalStream &&
-            <ReactPlayer className='rounded-md'
-              playing
-              muted
-              url={socketcontext.LocalStream} // Provide the actual URL here
-              height={'20vh'}
-              width={'20vw'}
-            />}
-        </div>
-      
+    <div className='h-screen w-screen relative flex flex-col justify-center items-center gap-5 p-3'>
+      <div className='h-max w-max p-2 ml-[80%] mb-[70%] absolute'>
+        {socketcontext?.LocalStream &&
+          <ReactPlayer className='rounded-md'
+            playing
+            muted
+            url={socketcontext.LocalStream} // Provide the actual URL here
+            height={'20vh'}
+            width={'20vw'}
+          />}
+      </div>
+
       <div className='h-max w-max p-2'>
         {socketcontext?.remoteStream && (
           <ReactPlayer className='rounded-md shadow-slate-300'
             playing
             muted
             url={socketcontext.remoteStream} // Provide the actual URL here
-            height={'90vh'}
+            height={'100%'}
             width={'90vw'}
           />
         )}
       </div>
-      <div className='mt-[100%] absolute flex w-screen h-[5vh] p-3 flex-row gap-5 bg-transparent'>
-        {/* {
-          socketcontext?.mycamera == true ? <Icon color='red' onClick={() => { socketcontext?.controlCamera() }} icon="pepicons-pop:camera-circle-off" height='5vh' />
-            : <Icon color='blue' onClick={() => { socketcontext?.controlCamera() }} icon="pepicons-pop:camera" height='5vh' />
-        }
-        {
-          socketcontext?.mymic == true ? <Icon color='red' onClick={() => { socketcontext?.controlMic() }} icon="bi:mic-mute-fill" height='5vh' />
-            : <Icon color='green' onClick={() => { socketcontext?.controlMic() }} icon="eva:mic-fill" height='5vh' />
-        } */}
-        <button onClick={()=>{socketcontext?.handleNegotiation()}} className='h-max w-max p-2 bg-gradient-r from-indigo-600 to-blue-600 font-semibold text-base'>Start Call</button>
+      <div className='mt-[100%] absolute flex w-screen h-[5vh] p-3 flex-row items-center justify-center gap-5 bg-transparent'>
+        <button onClick={() => { socketcontext?.handleNegotiation() }} className='h-max w-max p-2 bg-gradient-r text-white from-indigo-500 to-blue-500 font-semibold text-base'>Start Call</button>
       </div>
     </div>
   );
