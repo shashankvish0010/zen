027e3def9c573215438c3e1d95a82daf8ca88556
Shashank Vishwakarma <shashank000s4@gmail.com>
2024-02-07 22:13:48 +0530

    Updated: End Call

--- client/zen/src/pages/VideoCall.tsx
+++ client/zen/src/pages/VideoCall.tsx
@@ -1,15 +1,10 @@
 import React, { useContext } from 'react';
 import { Socketcontext } from '../context/Socketcontext';
 import ReactPlayer from 'react-player';
-// import { Icon } from '@iconify/react';
+import { Icon } from '@iconify/react';
 
 const VideoCall: React.FC = () => {
   const socketcontext = useContext(Socketcontext);
-
-  // useEffect(() => {
-  //   console.log(socketcontext?.mycamera, socketcontext?.mymic);
-  // }, [socketcontext]);
-
   return (
     <div className='h-screen w-screen relative flex flex-col justify-center items-center gap-5 p-3'>
       <div className='h-max w-max p-2 ml-[80%] mb-[70%] absolute'>
@@ -32,10 +27,12 @@ const VideoCall: React.FC = () => {
             height={'100%'}
             width={'90vw'}
           />
+
         )}
       </div>
       <div className='mt-[100%] absolute flex w-screen h-[5vh] p-3 flex-row items-center justify-center gap-5 bg-transparent'>
         <button onClick={() => { socketcontext?.handleNegotiation() }} className='h-max w-max p-2 bg-gradient-r text-white from-indigo-500 to-blue-500 font-semibold text-base'>Start Call</button>
+        <Icon onClick={() => { socketcontext?.endCall() }} className='bg-red-500 rounded-full p-2' icon="ic:round-call-end" color='white' height={'6vh'} />
       </div>
     </div>
   );
