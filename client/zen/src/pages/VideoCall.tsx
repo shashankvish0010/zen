c9805f20ddf695a5a113a3ed8bf0a738ef89eddb
Shashank Vishwakarma <shashank000s4@gmail.com>
2024-02-08 20:10:34 +0530

    Updated: UI

--- client/zen/src/pages/VideoCall.tsx
+++ client/zen/src/pages/VideoCall.tsx
@@ -6,8 +6,8 @@ import { Icon } from '@iconify/react';
 const VideoCall: React.FC = () => {
   const socketcontext = useContext(Socketcontext);
   return (
-    <div className='h-screen w-screen relative flex flex-col justify-center items-center gap-5 p-3'>
-      <div className='h-max w-max'>
+    <div className='relative h-screen w-screen flex flex-col items-center gap-5 p-3'>
+      <div className='absolute h-max w-max ml-[100%]'>
         {socketcontext?.LocalStream &&
           <ReactPlayer className='rounded-md'
             playing
@@ -18,7 +18,7 @@ const VideoCall: React.FC = () => {
           />}
       </div>
 
-      <div className='h-max w-max'>
+      <div className='relative h-max w-max'>
         {socketcontext?.remoteStream && (
           <ReactPlayer className='rounded-md shadow-slate-300'
             playing
@@ -29,10 +29,10 @@ const VideoCall: React.FC = () => {
           />
 
         )}
-      </div>
-      <div className='flex flex-col md:flex-row w-screen h-max p-3 items-center justify-center md:gap-5'>
-        <button onClick={() => { socketcontext?.handleNegotiation() }} className='h-max w-max p-2 bg-indigo-600 text-white font-semibold text-base'>Start Call</button>
-        <Icon onClick={() => { socketcontext?.endCall() }} className='bg-red-500 rounded-full p-2' icon="ic:round-call-end" color='white' height={'6vh'} />
+        <div className='absolute flex flex-col md:flex-row w-screen h-max p-3 items-center justify-center md:gap-5'>
+          <button onClick={() => { socketcontext?.handleNegotiation() }} className='h-max w-max p-2 bg-indigo-600 text-white font-semibold text-base'>Start Call</button>
+          <Icon onClick={() => { socketcontext?.endCall() }} className='bg-red-500 rounded-full p-2' icon="ic:round-call-end" color='white' height={'6vh'} />
+        </div>
       </div>
     </div>
   );
