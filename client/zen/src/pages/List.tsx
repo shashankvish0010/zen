70bee7d3fa7131300799d3b2c8223dd38287498c
Shashank Vishwakarma <shashank000s4@gmail.com>
2024-02-10 19:17:49 +0530

    Ui Changed

--- client/zen/src/pages/List.tsx
+++ client/zen/src/pages/List.tsx
@@ -19,7 +19,7 @@ const List: React.FC = () => {
   const [message, setMessage] = useState<string | null>(null)
   const saveList = async (id: string | undefined) => {
     try {
-      const response = await fetch('https://zen-backend-6acy.onrender.com'+'/add/tozenlist/' + id, {
+      const response = await fetch('https://zen-backend-6acy.onrender.com' + '/add/tozenlist/' + id, {
         method: "POST",
         headers: {
           "Content-Type": "application/json"
@@ -71,12 +71,130 @@ const List: React.FC = () => {
         <span onClick={() => saveList(id)} className='bg-blue-600 flex flex-row items-center gap-2 text-white font-medium rounded shadow p-2'><Icon icon="gridicons:add" color='white' /> Add Contact </span>
       </div>
       <div className='h-max w-max flex flex-wrap items-center justify-evenly gap-7'>
+        <div className="overflow-hidden border rounded-lg">
+          <table className="min-w-full divide-y divide-gray-200">
+            <thead className="bg-gray-50">
+              <tr>
+                <th
+                  scope="col"
+                  className="px-6 py-3 text-xs font-bold text-left text-gray-500 uppercase "
+                >
+                  Name
+                </th>
+                <th
+                  scope="col"
+                  className="px-6 py-3 text-xs font-bold text-left text-gray-500 uppercase "
+                >
+                  Zen No.
+                </th>
+                <th
+                  scope="col"
+                  className="px-6 py-3 text-xs font-bold text-right text-gray-500 uppercase "
+                >
+                  Status
+                </th>
+                <th
+                  scope="col"
+                  className="px-6 py-3 text-xs font-bold text-right text-gray-500 uppercase "
+                >
+                  Action
+                </th>
+              </tr>
+            </thead>
+            <tbody className="divide-y divide-gray-200">
+              <tr>
+                <td className="px-6 py-4 text-sm font-medium text-gray-800 whitespace-nowrap">
+                  1
+                </td>
+                <td className="px-6 py-4 text-sm text-gray-800 whitespace-nowrap">Jone Doe</td>
+                <td className="px-6 py-4 text-sm text-gray-800 whitespace-nowrap">
+                  jonne62@gmail.com
+                </td>
+                <td className="px-6 py-4 text-sm font-medium text-right whitespace-nowrap">
+                  <a className="text-green-500 hover:text-green-700" href="#">
+                    Edit
+                  </a>
+                </td>
+                <td className="px-6 py-4 text-sm font-medium text-right whitespace-nowrap">
+                  <a className="text-red-500 hover:text-red-700" href="#">
+                    Delete
+                  </a>
+                </td>
+              </tr>
+              <tr>
+                <td className="px-6 py-4 text-sm font-medium text-gray-800 whitespace-nowrap">
+                  2
+                </td>
+                <td className="px-6 py-4 text-sm text-gray-800 whitespace-nowrap">Jone Doe</td>
+                <td className="px-6 py-4 text-sm text-gray-800 whitespace-nowrap">
+                  jonne62@gmail.com
+                </td>
+                <td className="px-6 py-4 text-sm font-medium text-right whitespace-nowrap">
+                  <a className="text-green-300 hover:text-green-700" href="#">
+                    Edit
+                  </a>
+                </td>
+                <td className="px-6 py-4 text-sm font-medium text-right whitespace-nowrap">
+                  <a className="text-red-500 hover:text-red-700" href="#">
+                    Delete
+                  </a>
+                </td>
+              </tr>
+              <tr>
+                <td className="px-6 py-4 text-sm font-medium text-gray-800 whitespace-nowrap">
+                  3
+                </td>
+                <td className="px-6 py-4 text-sm text-gray-800 whitespace-nowrap">Jone Doe</td>
+                <td className="px-6 py-4 text-sm text-gray-800 whitespace-nowrap">
+                  jonne62@gmail.com
+                </td>
+                <td className="px-6 py-4 text-sm font-medium text-right whitespace-nowrap">
+                  <a className="text-green-500 hover:text-green-700" href="#">
+                    Edit
+                  </a>
+                </td>
+                <td className="px-6 py-4 text-sm font-medium text-right whitespace-nowrap">
+                  <a className="text-red-500 hover:text-red-700" href="#">
+                    Delete
+                  </a>
+                </td>
+              </tr>
+              <tr>
+                <td className="px-6 py-4 text-sm font-medium text-gray-800 whitespace-nowrap">
+                  4
+                </td>
+                <td className="px-6 py-4 text-sm text-gray-800 whitespace-nowrap">
+                  Mary Poppins
+                </td>
+                <td className="px-6 py-4 text-sm text-gray-800 whitespace-nowrap">
+                  marypoppins@gmail.com
+                </td>
+                <td className="px-6 py-4 text-sm font-medium text-right whitespace-nowrap">
+                  <a className="text-green-300 hover:text-green-700" href="#">
+                    Edit
+                  </a>
+                </td>
+                <td className="px-6 py-4 text-sm font-medium text-right whitespace-nowrap">
+                  <a className="text-red-500 hover:text-red-700" href="#">
+                    Delete
+                  </a>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
         {socketcontext?.zenList ? socketcontext.zenList.data.map((list: userType) =>
           <div key={list.id} className='border-2 border-gray-200 p-5 rounded-xl h-[10vh] w-[85vw] flex flex-row justify-evenly items-center gap-3'>
             <Icon icon="material-symbols:person" height='4vh' />
-            <span className='text-base font-semibold'>{list.firstname}</span>
-            <span className='text-md font-semibold'>{list.zen_no}</span>
-            <span onClick={() => { socketcontext?.calling(list.zen_no); navigate('/calling/' + list.zen_no) }} className='flex justify-center items-center gap-2 shadow-md cursor-pointer h-max w-max p-2 bg-blue-600 font-semibold text-base text-white rounded'> <Icon icon="ri:live-fill" /><p>Call</p></span>
+            <td className="px-6 py-4 text-sm font-medium text-right whitespace-nowrap">
+              {list.firstname}
+            </td>
+            <td className="px-6 py-4 text-sm font-medium text-right whitespace-nowrap">
+              {list.zen_no}
+            </td>
+            <td className="px-6 py-4 text-sm font-medium text-right whitespace-nowrap">
+              <span onClick={() => { socketcontext?.calling(list.zen_no); navigate('/calling/' + list.zen_no) }} className='flex justify-center items-center gap-2 shadow-md cursor-pointer h-max w-max p-2 bg-blue-600 font-semibold text-base text-white rounded'> <Icon icon="ri:live-fill" /><p>Call</p></span>
+            </td>
           </div>
         ) : null
         }
