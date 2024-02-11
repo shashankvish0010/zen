251cc0cebe07bc48fd33b78b1e6b82569c74e46b
Shashank Vishwakarma <shashank000s4@gmail.com>
2024-02-11 22:15:45 +0530

    Fixed: SQL concatenation operator to array append

--- server/src/routers/routes.ts
+++ server/src/routers/routes.ts
@@ -201,8 +201,8 @@ router.post('/add/tozenlist/:id', async (req, res) => {
             const userData = await pool.query('SELECT zen_list from Users WHERE id=$1', [id])            
             if (IszenNoValid.rows.length > 0 && userData.rows.length) {
                 // const listArray: string[] = userData.rows[0].zen_list
-                // const users = await pool.query('UPDATE Users SET zen_list=$2 WHERE id=$1', [id, `{"${zenNo}"}`])
-                const result = await pool.query('UPDATE Users SET zen_list=ARRAY_APPEND(zen_list, $1) WHERE id=$2', [`{"${zenNo}"}`, id]) 
+                const result = await pool.query('UPDATE Users SET zen_list=zen_list || $2 WHERE id=$1', [id, `{"${zenNo}"}`])
+                // const result = await pool.query('UPDATE Users SET zen_list=ARRAY_APPEND(zen_list, $1) WHERE id=$2', [`{"${zenNo}"}`, id]) 
                 console.log(result);
                 
                 if (result) {
