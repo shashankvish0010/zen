1c0ae0590f3391f17d532b64492854b7759c09d3
Shashank Vishwakarma <shashank000s4@gmail.com>
2024-02-11 23:19:53 +0530

    Added: User obj to apped at list

--- server/src/routers/routes.ts
+++ server/src/routers/routes.ts
@@ -199,15 +199,19 @@ router.post('/add/tozenlist/:id', async (req, res) => {
         if (zenNo) {
             console.log(zenNo);
             const IszenNoValid = await pool.query('SELECT zen_no from Users WHERE zen_no=$1', [zenNo]);
-            // if (IszenNoValid.rows.length > 0) {
-                // const result = await pool.query('UPDATE Users SET zen_list=zen_list || $2 WHERE id=$1', [id, `{"${zenNo}"}`])
-                const result = await pool.query('UPDATE Users SET zen_list=ARRAY_APPEND(zen_list, $1) WHERE id=$2', [`{"${zenNo}"}`, id])                 
+            if (IszenNoValid.rows.length > 0) {
+                const userData = await pool.query('SELECT firstname from Users WHERE zen_no=$1', [zenNo])
+                const user = {
+                    firstname : userData.rows[0].firstname,
+                    zen_no : zenNo
+                }
+                const result = await pool.query('UPDATE Users SET zen_list=ARRAY_APPEND(zen_list, $1) WHERE id=$2', [{user}, id])                 
                 if (result) {
                     res.json({ success: true, message: 'Added Successfully' })
                 }else{
                     res.json({ success: false, message: 'Not Added' })
                 }
-            // }
+            }
         }
     } catch (error) {
         console.log(error);
