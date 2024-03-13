069ba2624858cf87deac93d7412c765a878817ce
Shashank Vishwakarma <shashank000s4@gmail.com>
2024-03-13 17:25:43 +0530

    Changes: Ui for video call

--- client/zen/src/pages/VideoCall.tsx
+++ client/zen/src/pages/VideoCall.tsx
@@ -6,29 +6,31 @@ import { Icon } from '@iconify/react';
 const VideoCall: React.FC = () => {
   const socketcontext = useContext(Socketcontext);
   return (
-    <div className='absoulte h-screen w-screen flex flex-col justify-evenly items-center gap-5 p-5'>
-      <div className='h-max w-max'>
-        {socketcontext?.LocalStream &&
-          <ReactPlayer
-            playing
-            muted
-            url={socketcontext.LocalStream} // Provide the actual URL here
-            height={'30vh'}
-            width={'20vw'}
-          />}
-      </div>
+    <div className='h-max w-screen flex flex-col justify-evenly items-center gap-5 p-5'>
+      <div className='relative h-max w-max p-5 rounded-xl border-2'>
+        <div className='ml-[80%] absolute h-max w-max'>
+          {socketcontext?.LocalStream &&
+            <ReactPlayer
+              playing
+              muted
+              url={socketcontext.LocalStream} // Provide the actual URL here
+              height={'20vh'}
+              width={'20vw'}
+            />}
+        </div>
 
-      <div className='relative flex flex-col h-[50vh] w-max'>
-        {socketcontext?.remoteStream && (
-          <ReactPlayer className='rounded-md shadow-slate-300'
-            playing
-            muted
-            url={socketcontext.remoteStream} // Provide the actual URL here
-            height={'80vh'}
-            width={'85vw'}
-          />
+        <div className='flex flex-col h-[50vh] w-max'>
+          {socketcontext?.remoteStream && (
+            <ReactPlayer className='rounded-md shadow-slate-300'
+              playing
+              muted
+              url={socketcontext.remoteStream} // Provide the actual URL here
+              height={'50vh'}
+              width={'85vw'}
+            />
 
-        )}
+          )}
+        </div>
       </div>
       <div className='flex flex-col md:flex-row w-screen h-max p-3 items-center justify-center md:gap-5'>
         <button onClick={() => { socketcontext?.handleNegotiation() }} className='h-max w-max p-2 bg-indigo-600 text-white font-semibold text-base'>Start Call</button>
