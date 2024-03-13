733f2f4e0a9b962deb69a90e48d37c80313f440c
Shashank Vishwakarma <shashank000s4@gmail.com>
2024-03-13 17:18:59 +0530

    bug fixed

--- client/zen/src/pages/VideoCall.tsx
+++ client/zen/src/pages/VideoCall.tsx
@@ -6,7 +6,7 @@ import { Icon } from '@iconify/react';
 const VideoCall: React.FC = () => {
   const socketcontext = useContext(Socketcontext);
   return (
-    <div className='h-screen w-screen flex flex-col items-center gap-5'>
+    <div className='absoulte h-screen w-screen flex flex-col items-center gap-5'>
       <div className='h-max w-max'>
         {socketcontext?.LocalStream &&
           <ReactPlayer
@@ -14,18 +14,18 @@ const VideoCall: React.FC = () => {
             muted
             url={socketcontext.LocalStream} // Provide the actual URL here
             height={'30vh'}
-            width={'90vw'}
+            width={'20vw'}
           />}
       </div>
 
-      <div className='flex flex-col relative h-[50vh] w-max'>
+      <div className='relative flex flex-col h-[50vh] w-max'>
         {socketcontext?.remoteStream && (
           <ReactPlayer className='rounded-md shadow-slate-300'
             playing
             muted
             url={socketcontext.remoteStream} // Provide the actual URL here
-            height={'50vh'}
-            width={'90vw'}
+            height={'80vh'}
+            width={'85vw'}
           />
 
         )}
