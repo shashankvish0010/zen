9021f0fc167f2b1b131a62e54a7c448a8d71b6b3
Shashank Vishwakarma <shashank000s4@gmail.com>
2024-03-15 19:24:59 +0530

    Final Take

--- client/zen/src/components/Header.tsx
+++ client/zen/src/components/Header.tsx
@@ -9,26 +9,26 @@ const Header: React.FC = () => {
   const usercontext = useContext(UserContext)
   return (
     <>
-    <div className='h-[10vh] w-[100vw] flex flex-row justify-around items-center shadow'>
-      <Link to='/'>
-        <div className='text-2xl flex gap-1'>
-          <Icon icon="simple-icons:zenn" color='#9333ea' />
-          <h1 className='logo'>ZEN</h1>
+      <div className='h-[10vh] w-[100vw] flex flex-row justify-around items-center shadow'>
+        <Link to='/'>
+          <div className='text-2xl flex gap-1'>
+            <Icon icon="simple-icons:zenn" color='#9333ea' />
+            <h1 className='logo'>ZEN</h1>
+          </div>
+        </Link>
+        <div className='w-[30vw]'>
+          <ul className='hidden md:flex justify-between gap-2 items-center font-semibold'>
+            <Link to='/'>Home</Link>
+            <Link to='/about'>About</Link>
+            <li onClick={() => window.location.href = '/https://portfolio-six-gold-87.vercel.app/'}>Contact</li>
+            {usercontext?.login == false ?
+              <Link to='/signup'><button className='text-white p-1.5 bg-purple-600 rounded-sm'>Login/Signup</button></Link>
+              :
+              <button onClick={() => { usercontext?.dispatch({ type: "LOGOUT" }) }} className='bg-purple-600 p-1.5 text-white rounded-sm'>Logout</button>
+            }
+          </ul>
         </div>
-      </Link>
-      <div className='w-[30vw]'>
-        <ul className='hidden md:flex justify-between gap-2 items-center font-semibold'>
-          <Link to='/'>Home</Link>
-          <Link to='/about'>About</Link>
-          <Link to='/contact'>Contact</Link>
-          {usercontext?.login == false ?
-            <Link to='/signup'><button className='text-white p-1.5 bg-purple-600 rounded-sm'>Login/Signup</button></Link>
-            :
-            <button onClick={() => { usercontext?.dispatch({ type: "LOGOUT" }) }} className='bg-purple-600 p-1.5 text-white rounded-sm'>Logout</button>
-          }
-        </ul>
-      </div>
-      {
+        {
           open == true ?
             <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1, rotate: 90 }} transition={{ behaviour: "smooth" }}
               onClick={() => setOpen(!open)} className='md:hidden block h-max w-max'>
@@ -40,21 +40,25 @@ const Header: React.FC = () => {
               <Icon icon="quill:hamburger" height={'2rem'} color='indigo' />
             </motion.div>
         }
-    </div>
-    {
-            open == true ?
-            <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ behaviour: "smooth" }}
-              className='absolute md:hidden block h-max w-screen bg-purple-600 items-center p-3'>
-              <ul className='h-[25vh] text-white uppercase header_list md:hidden flex flex-col justify-around text-sm'>
-                <Link to='/'>Home</Link>
-                <Link to='/about'>About</Link>
-                <Link to='/contact'>Contact</Link>
+      </div>
+      {
+        open == true ?
+          <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ behaviour: "smooth" }}
+            className='absolute md:hidden block h-max w-screen bg-purple-600 items-center p-3'>
+            <ul className='h-[25vh] text-white uppercase header_list md:hidden flex flex-col justify-around text-sm'>
+              <Link to='/'>Home</Link>
+              <Link to='/about'>About</Link>
+              <Link to='/contact'>Contact</Link>
+              {usercontext?.login == false ?
                 <Link to='/signup'>Login/Signup</Link>
-              </ul>
-            </motion.div>
-            : null
-        }
-        </>
+                :
+                <li onClick={() => { usercontext?.dispatch({ type: "LOGOUT" }) }}>Login/Signup</li>
+              }
+            </ul>
+          </motion.div>
+          : null
+      }
+    </>
   )
 }
 
